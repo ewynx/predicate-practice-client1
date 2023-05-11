9caae16e6edd4092eba5800709b7891f01cb80ee
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2023-05-10 18:08:59 -0600

    Trying to use sendTransaction on predicate instead of transfer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,44 +2,71 @@ import React, { Component, useEffect } from "react";
 import ReactDOM from "react-dom";
 import logo from './logo.svg';
 import './App.css';
-import { NativeAssetId, Predicate, Provider, Wallet, WalletUnlocked, bn } from 'fuels';
+import { Address, CoinStatus, JsonFlatAbi, NativeAssetId, Predicate, Provider, ScriptTransactionRequest, Wallet, WalletUnlocked, bn } from 'fuels';
 import { BackendAbi__factory } from './types';
 
 function App() {
   // Connect to testnet beta-3
   const provider = new Provider('https://beta-3.fuel.network/graphql');
-  const predicate = BackendAbi__factory.createInstance(provider);
-  console.log(predicate.address.toB256());
-
-  // Setup test wallet
-  // TODO *Add in private key of wallet that is funded*
-  const wallet = Wallet.fromPrivateKey("0x..", provider);
+  const PREDICATE_BYTECODE =
+  '0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8';
+const PREDICATE_ADDRESS = '0x27c54187841c60ac0ebafcf1f2778d30f9973865f9a4bcd959e137aa852c4375';
+const PREDICATE_ABI = {
+  types: [
+    {
+      typeId: 0,
+      type: 'bool',
+      components: null,
+      typeParameters: null,
+    },
+    {
+      typeId: 1,
+      type: 'b256',
+    },
+  ],
+  functions: [
+    {
+      inputs: [
+        {
+          name: 'data',
+          type: 1,
+          typeArguments: null,
+        },
+      ],
+      name: 'main',
+      output: {
+        name: '',
+        type: 0,
+        typeArguments: null,
+      },
+    },
+  ],
+  loggedTypes: [],
+  configurables: [],
+};
+  const b256 = '0x0101010101010101010101010101010101010101010101010101010101010101';
+  const predicate = new Predicate(PREDICATE_BYTECODE, PREDICATE_ABI, provider);
 
   useEffect(() => {
     const connectPredicate = async () => {
-      // Fund the predicate
-      const response1 = await wallet.transfer(predicate.address, 100000, NativeAssetId, {
-        gasLimit: 164,
-        gasPrice: 1,
+      // Reference test code: https://github.com/FuelLabs/fuels-ts/blob/eda13d72c32f72652a34f926c4b9cf42ac36556c/packages/predicate/src/predicate.test.ts#L96
+      predicate.setData<[string]>(b256);
+
+      const request = new ScriptTransactionRequest();
+    
+      request.addResource({
+        // id: '0x01' gives error "Invalid b256 (argument="b256", value="0x01", code=INVALID_ARGUMENT..""
+        // this input gives error "Invalid struct UtxoId. Field "outputIndex" not present. (argument="UtxoId", value"
+        id: '0x0000000000000000000000000000000000000000000000000000000000000001',
+        assetId: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        amount: bn(1),
+        owner: Address.fromB256(PREDICATE_ADDRESS),
+        status: CoinStatus.Unspent,
+        maturity: 0,
+        blockCreated: bn(0),
       });
-      await response1.wait();
-
-      // Try to spend the funds in the predicate
-      const walletBalanceBefore = await wallet.getBalance();
-      console.log("walletBalanceBefore", walletBalanceBefore);
-
-      const tx = await predicate
-        .setData({has_account: true, total_complete: 100})
-        .transfer(wallet.address, 30, NativeAssetId, {
-          gasLimit: 3000,
-          gasPrice: 1,
-        });
-      await tx.waitForResult();
-
-      // This balance should be greater than "walletBalanceBefore", if the predicate was successfully spent
-      const walletBalanceAfter = await wallet.getBalance();
-      console.log("walletBalanceAfter", walletBalanceAfter);
-      
+    
+      predicate.sendTransaction(request);
     }
 
     connectPredicate()
